e3d53d41292f83848106ce2faf9b11da544e3151
git-sahi <bishtsahi.95@gmail.com>
2025-07-03 13:28:18 +0530

    Delete Movie Genre Classification.py

--- Movie Genre Classification.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import re
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
